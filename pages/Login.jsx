8425e5f1a8e5c0a3b539e1b527076caedaefd1f0
hari960619 <haridir150@gmail.com>
2026-01-25 00:31:05 +0530

    test login page

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -13,8 +13,8 @@ export default function PageUserLogin() {
   const [login, setLogin] = useState(false);
   const [error, setError] = useState("");
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+    email: "Check",
+    password: "123",
   });
 
   const { setUserView } = useContext(UserSettingViewContext);
