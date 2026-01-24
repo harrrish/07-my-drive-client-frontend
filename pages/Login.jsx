29df8a750910ace213355b26de7162a441328031
hari960619 <haridir150@gmail.com>
2026-01-24 22:44:17 +0530

    testing final client

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -13,8 +13,8 @@ export default function PageUserLogin() {
   const [login, setLogin] = useState(false);
   const [error, setError] = useState("");
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+    email: "Dummy",
+    password: "0",
   });
 
   const { setUserView } = useContext(UserSettingViewContext);
