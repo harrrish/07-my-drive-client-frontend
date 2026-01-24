bb2026e34f4e30d55dcd0942c57c445bf672a880
hari960619 <haridir150@gmail.com>
2026-01-24 22:10:38 +0530

    frontend deployment final

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -13,8 +13,8 @@ export default function PageUserLogin() {
   const [login, setLogin] = useState(false);
   const [error, setError] = useState("");
   const [formData, setFormData] = useState({
-    email: "latest",
-    password: "test",
+    email: "",
+    password: "",
   });
 
   const { setUserView } = useContext(UserSettingViewContext);
