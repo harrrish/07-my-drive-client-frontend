0d39080f3a6a21e873b609c7c74b12dfb3b1ee37
hari960619 <haridir150@gmail.com>
2026-01-26 12:00:05 +0530

    client deploy test 2

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -13,7 +13,7 @@ export default function PageUserLogin() {
   const [login, setLogin] = useState(false);
   const [error, setError] = useState("");
   const [formData, setFormData] = useState({
-    email: "test 1",
+    email: "test 2",
     password: "qwerty@12345",
   });
 
